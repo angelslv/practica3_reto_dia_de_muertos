ae7e31b75a9674a36b60762519a4a7110d804fae
Dr. Ángel Vásquez <angel.vasquez@tecnologiasmexico.com>
2020-10-28 09:25:58 -0600

    Se agregan datos para maquetar el HTML de la arania que estamos desarrollando. y se agrega el archivo CSS que vamos a comenzar a trabajar

---
+++ p3/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>PRÁCTICA 3. UMAR DÍA DE MUERTOS TECNOLOGÍAS DE INFORMACIÓN I.</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="GIT, PHP, HTML5, ARAÑAS">
+    <meta name="keywords" content="GIT, PHP, HTML5, ARAÑAS">
+    <meta name="author" content="Dr. Angel Vasquez">
+    <link rel="stylesheet" href="css/estilo.css" type="text/css">
+</head>
+<body>
+    <div class="arania">
+            <div class="araniaweb"></div>
+        <div class="cuerpo">
+            <div class="ojo izquierdo"></div>
+            <div class="ojo derecho"></div>
+        </div>  
+        <div class="patitas izquierda">
+            <div class="patas"></div>
+            <div class="patas"></div>
+            <div class="patas"></div>
+        </div>
+        <div class="patitas derecha">
+            <div class="patas"></div>
+            <div class="patas"></div>
+            <div class="patas"></div>
+        </div>
+    </div>
+</body>
+</html>
